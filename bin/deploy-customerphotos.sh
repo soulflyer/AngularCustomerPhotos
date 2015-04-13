979b545b07604cfdb326415672b967f4cd5fe1c1
Iain <public@soulflyer.co.uk>
2015-04-13 11:42:34 +0700

    exclude zip and json dirs from deploy script

--- bin/deploy-customerphotos.sh
+++ bin/deploy-customerphotos.sh
@@ -1,4 +1,5 @@
 echo "Copying files to soulflyer"
 rsync -vaikL  --exclude=.DS_Store --exclude=.sass-cache --exclude=.git \
+    --exclude=zip --exclude=json \
     /Users/iain/Code/Play/angular/CustomerPhotos/ \
         jphuquoc@soulflyer.co.uk:CustomerPhotos/
